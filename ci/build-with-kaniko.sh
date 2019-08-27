2ffc5c8b5dfca98427b7f19dab98f4cb0b653cb2
David Young <davidy@funkypenguin.co.nz>
2019-08-27 14:49:11 +1200

    Do we need path in kaniko now?

--- ci/build-with-kaniko.sh
+++ ci/build-with-kaniko.sh
@@ -1,15 +1,9 @@
 #!/busybox/sh
-set -x
-
-pwd
-
-# Retrieve the build directory which can vary across concourse workers
-DIR=`ls /tmp/build/`
 
 # Perform the build using Kaniko
 /kaniko/executor  \
---dockerfile=/tmp/build/$DIR/git-source/$COMPONENT/Dockerfile \
+--dockerfile=git-source/$COMPONENT/Dockerfile \
 --destination=doesnt/matter:we-are-not-pushing \
---context=/tmp/build/$DIR/git-source/$COMPONENT/ \
---no-push
---tarPath=/tmp/build/$DIR/image/image.tar
+--context=git-source/$COMPONENT/ \
+--no-push \
+--tarPath=image/image.tar
